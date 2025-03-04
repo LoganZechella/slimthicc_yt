80bd796a8bd56025af030b5854fe67f166c340f8
Logan Zechella <jloganzechella@gmail.com>
2025-03-04 17:14:57 -0500

    Download fail fix 5

--- server/src/services/download_strategies/spotify_strategy.py
+++ server/src/services/download_strategies/spotify_strategy.py
@@ -751,7 +751,7 @@ class SpotifyStrategy(DownloadStrategy):
                             "--user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
                             "--socket-timeout", "60",
                             "--retry-sleep", "3",
-                            "--max-retries", "5",
+                            "--retries", "5",
                             "--geo-bypass"
                         ])
                     
